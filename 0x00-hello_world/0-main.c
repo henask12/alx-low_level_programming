cbde89da8cd8dd81117b5adb60ee102e2ae927f1
Henok Mekonnen <Henokmekonnen5@gmail.com>
2022-06-02 12:55:16 -0700

    p

---
+++ 0x00-hello_world/0-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("Holberton School");
+
+			return (0);
+
+}
