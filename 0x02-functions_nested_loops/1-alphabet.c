2296a89110cd0f25761878223b269f514bf173c7
Henok Mekonnen <Henokmekonnen5@gmail.com>
2022-06-07 09:44:42 -0700

    çommit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,8 @@
 #include "main.h"
 /**
- *
- *  * print_alphabet - print alphabet lowercase
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ *print_alphabet - print alphabet lowercase
+ *Return: Always 0.
+ **/
 void print_alphabet(void)
 {
 	char alphabet;
