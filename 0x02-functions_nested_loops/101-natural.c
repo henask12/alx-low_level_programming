4555ef3ff97faca2506654168bab2358438360c2
Henok Mekonnen <Henokmekonnen5@gmail.com>
2022-06-07 21:20:51 -0700

    f

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 /**
- * main - computes and prints the sum of all the multiples
- * of 3 or 5 below 1024
- * Return: Always 0 (Success)
-**/
+ * main - increasing and decreasing until 98
+ * Return: Always 0.
+ **/
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int i, result; /*Delcaring statements*/
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-	for (i = 0; i < 1024; ++i)
+	i = 0;
+	result = 0;
+	/*Start While*/
+	while (i < 1024)
 	{
-		if ((i % 3) == 0)
-		{
-			sum3 = sum3 + i;
-		}
-		else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
-		}
-	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+		if (i % 3 == 0 || i % 5 == 0) /*Start condition IF*/
+			result += i;
+		i++;
+	} /*END condition IF*/
+	printf("%d\n", result);
 	return (0);
 }
