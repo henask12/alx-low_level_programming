248794daabb7f9babec164cc340870ced2cc9a2f
Henok Mekonnen <Henokmekonnen5@gmail.com>
2022-06-07 21:44:52 -0700

    new change

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
-**/
+ **/
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -17,31 +17,30 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-					else if (k < 10 && j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k + '0');
-					}
-					else if (k >= 10 && k < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((k / 10) + '0');
-						_putchar((k % 10) + '0');
-						else if (k >= 100)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar((k / 100) + '0'');
-							_putchar(((k / 10) % 10) + '0');
-							_putchar((k % 10) + '0');
-						}
-					}
-					_putchar('\n');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
+	}
+}
