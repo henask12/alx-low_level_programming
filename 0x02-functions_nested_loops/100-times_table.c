585250c2d7dd8266f9dbcabae7b6acf6c44ac00b
Henok Mekonnen <Henokmekonnen5@gmail.com>
2022-06-07 21:49:22 -0700

    change

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * times_table - prints the n times table, starting with 0
+ *print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  **/
-void times_table(int n)
+void print_times_table(int n)
 {
 	int i, j, k;
 
